8a420e8520c44205461de4c908a92fbd6fab7030
AngleWangwang <819096210@qq.com>
2017-08-11 19:49:00 +0800

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,71 @@
+'use strict';
+//引入node配置模块
+const htmlWebpackPlugin = require("html-webpack-plugin");
+const path = require("path");
+
+module.exports = {
+    //引入入口文件
+    entry: "./src/js/main.js",
+    output: {
+        //输出的路径已经要写绝对路径
+        path: path.resolve(__dirname, "./dist"),
+        filename: "buld.js"
+    },
+    plugins: [
+        new htmlWebpackPlugin({
+            // template: "./src/index.html",
+            template: 'html-withimg-loader!' + path.resolve(__dirname, "./src/index.html"),
+            filename: "index.html",
+            inject: "body"
+        })
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: [
+                    'style-loader',
+                    'css-loader'
+                ]
+            },
+            {
+                test: /\.less$/,
+                use: [
+                    'style-loader',
+                    'css-loader',
+                    'less-loader'
+                ]
+            },
+            {
+                test: /\.scss$/,
+                use: [
+                    'style-loader',
+                    'css-loader',
+                    'sass-loader'
+                ]
+            },
+            {
+                test: /\.tpl$/,
+                use: [
+                    'html-loader'
+                ]
+            },
+            {
+                test: /\.(jpg|pbg|gif)$/,
+                use: [
+                    { loader: "url-loader", options: { limit: 25000 } },
+                    'image-webpack-loader'
+                ]
+            },
+            {
+                test: /\.vue$/,
+                use: [
+                    'vue-loader'
+                ]
+            }
+        ]
+
+    }
+
+
+}
